e1801419a18c352979b0da0c11b41f5b9112a3c7
Saperspecnaz <saperspecnaz12@gmail.com>
2026-02-22 21:53:02 +0300

    finale fix??

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/plus', (req, res) => {
   if (!x || !y) return res.send('Add x and y as query parameters')
 
   const solution = plus(Number(x), Number(y))
-  res.send("///dd " + solution)
+  res.send("///dxxd " + solution)
 })
 
 app.get('*', function (_req, res) {
